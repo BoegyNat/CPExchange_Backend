998265504952ede0b8a604733150c33ad67aa621
BT_kung <u.nantanat@gmail.com>
2024-05-23 16:36:10 +0700

    Add PostNewDriver, EditDriver and DeleteDriver and Add Driver in database

--- app/controllers/driver.controller.js
+++ app/controllers/driver.controller.js
@@ -1,28 +1,137 @@
 const db = require("../models");
+const pool = require("../connection.js");
 const Drivers = db.drivers;
 
-exports.allDrivers = (req, res) => {
+exports.allDrivers = async (req, res) => {
   try {
-    let result = Drivers.filter((driver) => driver.IsActive == 0);
-    if(result){
+    // let result = Drivers.filter((driver) => driver.IsActive == 0);
+    const result = await pool.query("SELECT * FROM Driver WHERE IsActive = 0");
+    if (result) {
       res.status(200).send(result);
     }
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
-
 };
 
-exports.deleteDrivers = (req, res) => {
+exports.postNewDriver = async (req, res) => {
   try {
-    Drivers.forEach((driver) => {
-      if(req.body.idDrivers.indexOf(driver.idDriver) > -1){
-        driver.IsActive = -1;
-      }
-    });
-    res.status(200).send();
+    const newDriver = req.body;
+    console.log(newDriver);
+    const result = await pool.query(
+      `
+      INSERT INTO 
+      Driver 
+          (PreName,
+          FullName,
+          Telephone,
+          AgeYear,
+          AgeMonth,
+          AgeWorkingYear,
+          AgeWorkingMonth,
+          Company,
+          WorkingCompany,
+          Salary,
+          CostCenter,
+          CostElement,
+          IsActive,
+          Rating
+        )
+      VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?,?)
+      `,
+      [
+        newDriver.PreName,
+        newDriver.FullName,
+        newDriver.Telephone,
+        newDriver.AgeYear,
+        newDriver.AgeMonth,
+        newDriver.AgeWorkingYear,
+        newDriver.AgeWorkingMonth,
+        newDriver.Company,
+        newDriver.WorkingCompany,
+        newDriver.Salary,
+        newDriver.CostCenter,
+        newDriver.CostElement,
+        0,
+        0,
+      ]
+    );
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ message: error.message });
+    }
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
+};
 
+exports.postEditDriver = async (req, res) => {
+  try {
+    const newDriver = req.body[0];
+    const idDriver = req.body[1];
+    const result = await pool.query(
+      `
+      UPDATE 
+      Driver SET 
+      PreName = ?,
+      FullName = ?,
+      Telephone = ?,
+      AgeYear  = ?,
+      AgeMonth = ?,
+      AgeWorkingYear  = ?,
+      AgeWorkingMonth = ?,
+      Company = ?,
+      WorkingCompany = ?,
+      Salary = ?,
+      CostCenter = ?,
+      CostElement = ? 
+      WHERE 
+        idDriver = ?`,
+      [
+        newDriver.PreName,
+        newDriver.FullName,
+        newDriver.Telephone,
+        newDriver.AgeYear,
+        newDriver.AgeMonth,
+        newDriver.AgeWorkingYear,
+        newDriver.AgeWorkingMonth,
+        newDriver.Company,
+        newDriver.WorkingCompany,
+        newDriver.Salary,
+        newDriver.CostCenter,
+        newDriver.CostElement,
+        idDriver,
+      ]
+    );
+    if (result) {
+      res.status(200).send(result);
+    } else {
+      res.status(404).send({ message: error.message });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+};
+
+exports.deleteDrivers = async (req, res) => {
+  try {
+    const deleteDrivers = req.body.idDrivers;
+    const rows = [];
+    for (let i = 0; i < deleteDrivers.length; i++) {
+      const row = await pool.query(
+        `UPDATE Driver SET IsActive = ? WHERE idDriver = ?`,
+        [-1, deleteDrivers[i]]
+      );
+      rows.push(row);
+    }
+    if (rows) {
+      res.status(200).send(rows);
+    } else {
+      res.status(404).send({ message: error.message });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 };
--- app/routes/driver.route.js
+++ app/routes/driver.route.js
@@ -1,25 +1,32 @@
 const { authJwt } = require("../middleware");
 const driverController = require("../controllers/driver.controller");
 
-module.exports = function(app) {
-    app.use(function(req, res, next) {
-      res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-      );
-      next();
-    });
-  
-    app.get(
-      "/api/drivers",
-      [authJwt.verifyToken],
-      driverController.allDrivers
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
     );
+    next();
+  });
 
-    app.post(
-      "/api/drivers/delete",
-      [authJwt.verifyToken],
-      driverController.deleteDrivers
-    );
-  
+  app.get("/api/drivers", [authJwt.verifyToken], driverController.allDrivers);
+
+  app.post(
+    "/api/post_new_driver/",
+    [authJwt.verifyToken],
+    driverController.postNewDriver
+  );
+
+  app.post(
+    "/api/post_edit_driver/",
+    [authJwt.verifyToken],
+    driverController.postEditDriver
+  );
+
+  app.post(
+    "/api/post_delete_drivers/",
+    [authJwt.verifyToken],
+    driverController.deleteDrivers
+  );
 };
