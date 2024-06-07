91581c0a544252cf3951ec5cf5faddec72ba077c
BT_kung <u.nantanat@gmail.com>
2024-06-07 15:45:27 +0700

    Clean console.log in Emp

--- app/controllers/employee.controller.js
+++ app/controllers/employee.controller.js
@@ -9,7 +9,7 @@ exports.allEmployees = async (req, res) => {
       // "SELECT * FROM Employee ;"
       "SELECT * FROM UniHR.Employees e LEFT JOIN UniHR.EmployeePosition ep ON e.idEmployees = ep.idEmployees  LEFT JOIN UniHR.`Position` p ON ep.idPosition = p.idPosition LEFT JOIN UniHR.`Section` s ON p.idSection = s.idSection LEFT JOIN UniHR.Department d ON p.idDepartment = d.idDepartment LEFT JOIN UniHR.Division d2 ON p.idDivision = d2.idDivision LEFT JOIN UniHR.BusinessUnit bu ON p.idBusinessUnit = bu.idBusinessUnit LEFT JOIN UniHR.Company c ON p.idCompany = c.idCompany WHERE ep.`start` <= CURDATE() AND ep.`end` >= CURDATE() OR ep.`end` IS NULL ;"
     );
-    console.log(row);
+
     res.status(200).send(row);
   } catch (error) {
     res.status(500).send({ message: error.message });
