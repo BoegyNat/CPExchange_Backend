fbd29ca1b180d6cb7394e37c620911a689b8b43b
BT_kung <u.nantanat@gmail.com>
2024-06-13 03:33:52 +0700

    Fix bug in allSite

--- app/controllers/site.controller.js
+++ app/controllers/site.controller.js
@@ -1,11 +1,14 @@
 const pool = require("../connection.js");
 
 exports.getSiteByName = async (req, res) => {
-  const rows = await pool.query("SELECT * FROM ScgSite WHERE NoSite = ?", [
-    req.params.IdScgSite,
+  console.log(req.params.IdScgSite);
+  const NoSite = parseInt(req.params.IdScgSite);
+  console.log(NoSite);
+  const rows = await pool.query("SELECT * FROM ScgSite WHERE idScgSite = ?", [
+    NoSite,
   ]);
   // const rows = await pool.query("SELECT * FROM ScgSite")
-
+  console.log(rows);
   return res.status(200).json(rows);
 };
 
@@ -30,20 +33,20 @@ exports.addLocationDriver = async (req, res) => {
   }
 };
 exports.getLocationDriverById = async (req, res) => {
-  const rows = await pool.query("SELECT * FROM LocationDriver WHERE idDriver = ? ORDER BY idLocationDriver DESC LIMIT 1", [
-    req.params.idDriver,
-  ]);
+  const rows = await pool.query(
+    "SELECT * FROM LocationDriver WHERE idDriver = ? ORDER BY idLocationDriver DESC LIMIT 1",
+    [req.params.idDriver]
+  );
 
   res.status(200).send(rows);
 };
-exports.getAllSite = async (req,res) =>{
+exports.getAllSite = async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM ScgSite")
-    if(result){
-      return res.status(200).send(result)
-    }else{
-      return res.status(400).send({success: false,
-        error: error.message,})
+    const result = await pool.query("SELECT * FROM ScgSite");
+    if (result) {
+      return res.status(200).send(result);
+    } else {
+      return res.status(400).send({ success: false, error: error.message });
     }
   } catch (error) {
     return res.status(500).send({
@@ -51,4 +54,4 @@ exports.getAllSite = async (req,res) =>{
       error: error.message,
     });
   }
-}
+};
