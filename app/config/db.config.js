ff96f6ddc8cc2cd6acc2eac421dc3c53f28f3652
BoegyNat <bboegy@gmail.com>
2025-03-22 21:34:23 +0700

    Add .env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
   // HOST: "db", // ใช้ใน Docker
-  HOST: process.env.DB_HOST || "localhost",
-  USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "boegy5882",
-  DB: process.env.DB_NAME || "cpexchange",
-  PORT: process.env.DB_PORT || "3306",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  PORT: process.env.DB_PORT,
   // SOCKETPATH: process.env.DB_SOCKETPATH || "/var/run/mysqld/mysqld.sock", // ใช้ใน Linux หรือ Mac
 };
